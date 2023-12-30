9a66f990853cc106ac8e074e88ab5b2ae403f91e
Jose Augusto <jose_augusto2019@hotmail.com>
2023-12-30 15:29:08 -0300

    Criação do projeto

---
+++ src/main/java/br/com/rainschool/RainschoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.rainschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rainschool/RainschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rainschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
